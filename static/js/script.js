3127ead2184e946a9f2e2d2ed738ade51c717f13
Varun Singh <55821761+Iamvarunking@users.noreply.github.com>
2021-10-31 19:09:21 +0530

    rock paper scissors game added

--- static/js/script.js
+++ static/js/script.js
@@ -48,3 +48,72 @@ function generateCat(){
     image.src = "https://thecatapi.com/api/images/get?formet=src&type=gif&size=small";
     div.appendChild(image)
 }
+
+// Rock, Paper, Scissors
+
+function rpsGame(yourChoice){
+    let humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = botChoiceString(botChoiceInt());
+    console.log(botChoice);
+    let result = decideWinner(humanChoice,botChoice);
+    let message = finalMassage(result);
+    rpsFrontEnd(humanChoice, botChoice, message)
+    console.log(result);
+    console.log(message);
+}
+
+function botChoiceInt() {
+    return Math.floor(Math.random() * 3);
+}
+
+function botChoiceString(number) {
+    return ["rock", "paper", "scissors"][number];
+}
+
+function decideWinner(yourChoice,computerChoice){
+    let rpsDatabase = {
+        "rock":{"scissors":1, "rock":0.5, "paper":0},
+        "paper":{"rock":1, "paper":0.5, "scissors":0},
+        "scissors":{"paper":1, "scissors":0.5, "rock":0}
+    };
+
+    let yourScore = rpsDatabase[yourChoice][computerChoice];
+    let computerScore = rpsDatabase[computerChoice][yourChoice];
+    return [yourScore, computerScore] ;
+}
+
+function finalMassage([yourScore,computerScore]) {
+    if(yourScore === 0){
+        return { 'message': 'You lost!', 'color': 'red'};
+    } else if(yourScore === 0.5){
+        return { 'message': 'You tied', 'color': 'blue'};
+    } else {
+        return { 'message': 'You won!', 'color': 'green'};
+    }
+}
+
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMassage) {
+    let imagesDatabase = {
+        "rock": document.getElementById('rock').src,
+        "paper": document.getElementById('paper').src,
+        "scissors": document.getElementById('scissors').src,
+    }
+    document.getElementById("rock").remove();
+    document.getElementById("paper").remove();
+    document.getElementById("scissors").remove();
+
+    let humanImageDiv = document.createElement('div');
+    let botDiv = document.createElement('div');
+    let messageDiv = document.createElement('div');
+
+    humanImageDiv.innerHTML = "<img src='" +imagesDatabase[humanImageChoice]+ "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+    document.getElementById('flex-box-container-3').appendChild(humanImageDiv);
+
+    messageDiv.innerHTML = "<h1 style='color:"+finalMassage.color+"; font-size:60px; padding:30px'>"+finalMassage.message+"</h1>"
+    document.getElementById('flex-box-container-3').appendChild(messageDiv);
+
+    botDiv.innerHTML = "<img src='" +imagesDatabase[botImageChoice]+ "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+    document.getElementById('flex-box-container-3').appendChild(botDiv);
+}
